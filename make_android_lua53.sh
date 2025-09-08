0c78a86b75891175102aea437cd887e24e392b4c
wqtest <975788721@qq.com>
2025-09-08 15:01:25 +0800

    Update make_android_lua53.sh

--- make_android_lua53.sh
+++ make_android_lua53.sh
@@ -24,6 +24,6 @@ function build() {
     cp ${BUILD_PATH}/libxlua.so plugin_lua53/Plugins/Android/libs/${ABI}/libxlua.so
 }
 
-build android-16 armeabi-v7a arm-linux-androideabi-4.9
-build android-16 arm64-v8a  arm-linux-androideabi-clang
-build android-16 x86 x86-4.9
+build android-35 armeabi-v7a arm-linux-androideabi-4.9
+build android-35 arm64-v8a  arm-linux-androideabi-clang
+build android-53 x86 x86-4.9
